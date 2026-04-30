259efd0cd34d5274e7ae1db5f1627740d3e33df3
Steven Hicken <shick.pub@pm.me>
2026-04-30 13:59:53 -0700

    fix: grant logging writer to vm service account

--- terraform/main.tf
+++ terraform/main.tf
@@ -57,6 +57,12 @@ resource "google_service_account" "vm_sa" {
   project      = var.gcp_project_id
 }
 
+resource "google_project_iam_member" "vm_sa_logging_writer" {
+  project = var.gcp_project_id
+  role    = "roles/logging.logWriter"
+  member  = "serviceAccount:${google_service_account.vm_sa.email}"
+}
+
 # IAM: allow VMs to pull images from Artifact Registry
 resource "google_artifact_registry_repository_iam_member" "vm_pull_images" {
   location   = var.gcp_region
